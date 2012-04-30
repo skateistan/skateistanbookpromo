7ca05fc2da1ee1af7851dddd54ff4a8f43a847a9
James Dennes <jdennes@gmail.com>
2012-04-30 14:26:37 +0200

    Added pre-order subscribe form.

--- book.js
+++ book.js
@@ -12,18 +12,52 @@ $(function () {
     } else {
       show("news");
     }
+    // Set up slides
     $("#slides").slides({
       preload: false,
       generatePagination: false,
       bigTarget: true
     });
+    // Set up subscribe form
+    setupsubscribe();
   }
-  function hideAll() {
+  function setupsubscribe () {
+    $('#preorder-signup').submit(function (e) { return false; });
+    $('button#subscribe').click(function (e) {
+      var $subform = $("#preorder-signup");
+      $(this).addClass("disabled");
+      $.getJSON(
+        "http://skateistan.createsend.com/t/j/s/fuhhh/?callback=?",
+        $subform.serialize(),
+        function (data) {
+          if (data.Status === 400) {
+            $("button#subscribe").removeClass("disabled");
+            $("#message")
+              .addClass("error")
+              .html("Uh, please make sure you fill out the form properly.")
+              .slideDown()
+              .delay(5000)
+              .fadeOut();
+          } else { // 200
+            $("input#name").val("");
+            $("input#fuhhh-fuhhh").val("");
+            $("button#subscribe").removeClass("disabled");
+            $("#message")
+              .removeClass("error")
+              .html("Thanks - you're on the pre-order list and will hear from us soon!")
+              .slideDown()
+              .delay(8000)
+              .fadeOut();
+          }
+        });
+      });
+  }
+  function hideall() {
     $(".content").hide();
     $("#menu a").removeClass("current");
   }
   function show(id) {
-    hideAll();
+    hideall();
     $("#a-" + id).attr("class", "current");
     if (id === "news") {
       patterns();
