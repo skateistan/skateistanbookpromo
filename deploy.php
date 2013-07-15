b0104d22bbf5382ef6dac36ea5cd1b4efd1fe856
manue1 <manub1312@googlemail.com>
2013-07-15 13:25:10 +0200

    Add new GitHub Public IPs for webhooks / CIDR.

--- deploy.php
+++ deploy.php
@@ -1,12 +1,33 @@
 <?php
-  error_reporting(0);
+	error_reporting(0);
 
-  $ip = $_SERVER['REMOTE_ADDR'];
-  $github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178');
-  $p = json_decode($_REQUEST['payload']);
+	$ip = $_SERVER['REMOTE_ADDR'];
+	$github_cidrs = array('204.232.175.64/27', '192.30.252.0/22');
+	$p = json_decode($_REQUEST['payload']);
+	
+	#Is the request made from Github?
+	function isitgithub($ip, $cidr) {		 
+		$parts = explode('/', $cidr);
+		$ipc = explode('.', $parts[0]);
+		foreach ($ipc as &$v)
+			$v = str_pad(decbin($v), 8, '0', STR_PAD_LEFT);
+		$ipc = substr(join('', $ipc), 0, $parts[1]);
+		$ipu = explode('.', $ip);
+		foreach ($ipu as &$v)
+			$v = str_pad(decbin($v), 8, '0', STR_PAD_LEFT);
+		$ipu = substr(join('', $ipu), 0, $parts[1]);
+		return $ipu == $ipc;
+	}
+	
+	$validaddr = false;
+	foreach ($github_cidrs as $addr)
+		if (isitgithub($ip, $addr)) {
+			$validaddr = true;
+			break;
+		}
 
-  # Request must be made from GitHub and we only want to deploy master
-  if (in_array($ip, $github_ips) && $p->ref === 'refs/heads/master') {
-    `git fetch --all && git reset --hard origin/master`;
-  }
-?>
+	# Request must be made from GitHub and we only want to deploy master
+	if ($validaddr == 'true' && $p->ref === 'refs/heads/master') {
+		`git fetch --all && git reset --hard origin/master`;
+	}
+ ?>
